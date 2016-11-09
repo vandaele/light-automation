8abc13e7310775b53a182bea89d1285e1d586038
Julien Vandaele <julien.vandaele@inria.fr>
2016-11-09 11:22:54 +0100

    add a script to set all led panels on

---
+++ all.py
@@ -0,0 +1,25 @@
+from automaton_client import *
+
+SERVER_HOST = "localhost"
+SERVER_PORT = 1502
+
+try:
+    c = AutomatonClient(host=SERVER_HOST, port=SERVER_PORT)
+
+    # open or reconnect TCP to server
+    if not c.is_open():
+        if not c.open():
+            print("unable to connect to %s:%s" % (SERVER_HOST, SERVER_PORT))
+
+    # if open() is ok, read register (modbus function 0x03)
+    if c.is_open():
+        # read 10 registers at address 0, store result in regs list
+        result = c.write_all([100]*23)
+        # if success display registers
+        if result:
+            print("WROTE 23 regs from ad #12389")
+
+    c.close()
+
+except ValueError:
+    print("Error with host or port params")
